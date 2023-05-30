45601cdf5673751416d7ec761b39e2bb00c56560
mprachaya <m_babyboy23@hotmail.co.th>
2023-05-30 18:29:30 +0700

    test

--- app/api/url/partSetup.js
+++ app/api/url/partSetup.js
@@ -1,4 +1,4 @@
 module.exports = {
   // apiPart: 'http://localhost:3200/api/v1/',
-  apiPart: 'http://10.2.6.37:3200/api/v1/',
+  apiPart: 'http://10.2.6.50:3200/api/v1/',
 };
--- app/containers/Pages/StudyPlanPage/components/PlanDetailPage.js
+++ app/containers/Pages/StudyPlanPage/components/PlanDetailPage.js
@@ -13,38 +13,39 @@ import useFetch from '../../../../hooks/useFetch';
 import url from '../../../../api/url/partSetup';
 
 function PlanDetailPage() {
-  const [subPlanData, setSubjPlanData] = useState([]);
-  const [duration] = useState([
+  const [duration, setDuration] = useState([
     {
-      id: 1, year: 1, semester: 1, subplan: []
+      id: 0, year: 1, semester: 1, subplan: []
     },
     {
-      id: 2, year: 1, semester: 2, subplan: []
+      id: 1, year: 1, semester: 2, subplan: []
     },
     {
-      id: 3, year: 2, semester: 1, subplan: []
+      id: 2, year: 2, semester: 1, subplan: []
     },
     {
-      id: 4, year: 2, semester: 2, subplan: []
+      id: 3, year: 2, semester: 2, subplan: []
     },
     {
-      id: 5, year: 3, semester: 1, subplan: []
+      id: 4, year: 3, semester: 1, subplan: []
     },
     {
-      id: 6, year: 3, semester: 2, subplan: []
+      id: 5, year: 3, semester: 2, subplan: []
     },
     {
-      id: 7, year: 4, semester: 1, subplan: []
+      id: 6, year: 4, semester: 1, subplan: []
     },
     {
-      id: 8, year: 4, semester: 2, subplan: []
+      id: 7, year: 4, semester: 2, subplan: []
     },
   ]);
+  const [subPlanData, setSubPlanData] = useState([]);
+
   const { resData: AllSubPlan } = useFetch(`${url.apiPart + 'substudy'}`, { study_plan_id: JSON.parse(localStorage.getItem('study_plan_id')), sub_study_semester: '', sub_study_year: '' }); // select all subplan by studyplan to make add menu filter
 
   useEffect(() => {
     if (AllSubPlan.data !== undefined) {
-      setSubjPlanData(AllSubPlan.data);
+      setSubPlanData(AllSubPlan.data);
     }
   }, [AllSubPlan]);
 
@@ -54,13 +55,12 @@ function PlanDetailPage() {
       const resultFilter = duration.map(du => subPlanData.filter(sp => du.year === sp.sub_study_year && du.semester === sp.sub_study_semester)
         .map(filterPlan => filterPlan));
       // duration.forEach(index => { duration[index].subplan = resultFilter[index]; });
-      duration.forEach((val, index) => { duration[index].subplan = resultFilter[index]; }); // move result data to duration state
+      const updateSubplan = duration.map((obj, index) => (
+        obj.id === index && { ...obj, subplan: resultFilter[index] }));
+      setDuration(updateSubplan);
+      // duration.forEach((val, index) => { duration[index].subplan = resultFilter[index]; }); // move result data to duration state
     }
   }, [subPlanData]);
-
-  useEffect(() => {
-    console.log(duration);
-  }, [duration]);
 
   return (
     <div>
@@ -86,7 +86,7 @@ function PlanDetailPage() {
                 justifyContent: 'center',
               }}>
               <TableContainer component={Paper}>
-                {duration.map((d) => (
+                {duration ? duration.map((d) => (
                   <Table key={d.id} sx={{ minWidth: 650 }} size="small">
                     <tbody>
                       <TableRow>
@@ -103,17 +103,17 @@ function PlanDetailPage() {
                           <Typography fontSize={14} fontWeight={'bold'}>credit</Typography>
                         </TableCell>
                       </TableRow>
-                      {d.subplan ? d.subplan.map((data, index) => (
-                        <Fragment key={data.sub_study_id} >
+                      {d.subplan ? d.subplan.map((da, index) => (
+                        <Fragment key={da.sub_study_id} >
                           <TableRow sx={{ margin: 2, mx: 2 }}>
                             <TableCell align='left'>
-                              <Typography fontSize={14}>{data.subject_code}</Typography>
+                              <Typography fontSize={14}>{da.subject_code}</Typography>
                             </TableCell>
                             <TableCell align='left'>
-                              <Typography fontSize={14}>{data.subject_name_th}</Typography>
+                              <Typography fontSize={14}>{da.subject_name_th}</Typography>
                             </TableCell>
                             <TableCell align='right' sx={{ pr: 10 }}>
-                              <Typography fontSize={14}>{data.credit_qty}</Typography>
+                              <Typography fontSize={14}>{da.credit_qty}</Typography>
                             </TableCell>
                           </TableRow>
                           {d.subplan.length === index + 1 && <TableRow sx={{ margin: 2, mx: 2 }}><TableCell align='left' sx={{ fontWeight: 'bold' }}>total</TableCell><TableCell colSpan={2} align='right' sx={{ pr: 10, fontWeight: 600, color: 'steelblue' }}>{d.subplan.reduce((prev, current) => prev + current.credit_qty, 0)}</TableCell></TableRow>}
@@ -128,6 +128,7 @@ function PlanDetailPage() {
                     </tbody>
                   </Table>
                 ))
+                  : 'loading'
                 }
               </TableContainer>
             </Box>
